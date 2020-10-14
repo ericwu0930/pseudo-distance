8a13bcb9cb615fcfc5e8a5c762a9c9ebefabbb5e
erichwu <is.eric.wu@gmail.com>
2020-10-14 18:32:05 +0800

    new push

--- pdist.m
+++ pdist.m
@@ -4,13 +4,17 @@ function [dist]=pdist(link,R,vertexes,faces,n)
 % R 包络每个连杆的半径
 % vertexes 障碍平面顶点集合 nx3
 % faces 障碍平面 nx1的胞元数组
-%% 程序主体
+%% Body
 % 立方体面个数
 dist = inf;
 faceCnt = size(faces,1);
+% 遍历每一个障碍面
 for i = 1:faceCnt
+    % 遍历每一个连杆
     for j = 1:size(link,1)-1
+        % 提取面上的各个顶点
         plane = faces{i,1};
+        % 记录顶点个数
         vCnt = length(plane);
         cpps = zeros(vCnt,3);
         for k = 1:vCnt
